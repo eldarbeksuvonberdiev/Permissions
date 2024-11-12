398d528e512d337ed0ff066bc0a65a306fd73c4e
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-11-12 17:09:59 +0500

    Verification page tailwindga o'zgartirildi

--- resources/views/verify-email.blade.php
+++ resources/views/verify-email.blade.php
@@ -1,34 +1,15 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body style="background-color: rgb(32, 31, 31)">
-    <div class="container">
-        <div class="row">
-            <div class="col-6 offset-3" style="background-color:aliceblue;margin-top:30vh;border-radius:20px">
-                <a type="submit" href="{{ route('dashboard') }}" class="btn btn-primary mb-5 mt-3">Back</a>
-                <form action="{{ route('email.verify') }}" method="POST">
-                    @csrf
-                    <div class="mb-5">
-                        <label for="code" class="form-label">verification that sent to your registered
-                            email</label>
-                        <input type="number" class="form-control" name="code" id="code">
-                    </div>
-                    <button type="submit" class="btn btn-primary mb-5">Submit</button>
-                </form>
-            </div>
+<x-guest-layout>
+    <form method="POST" action="{{ route('email.verify') }}">
+        @csrf
+        <div>
+            <x-input-label for="code" :value="__('Email Verification Code')" />
+            <x-text-input id="code" class="block mt-1 w-full" type="text" name="code" :value="old('code')" required autofocus autocomplete="code" />
+            <x-input-error :messages="$errors->get('code')" class="mt-2" />
         </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+        <div class="flex items-center justify-end mt-4">
+            <x-primary-button class="ms-4">
+                {{ __('Verify') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
